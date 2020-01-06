f99632a613a307cb4cd02d038e881c7b77625afb
sachavincent <Alykas88@gmail.com>
2020-01-06 02:37:00 +0100

    FenetreServeur amélioré

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,7 @@ package main;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GraphicsEnvironment;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
@@ -12,6 +13,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import database.DBConnection;
+import view.FenetreServeur;
 
 /**
  * TODO Renommer cette classe parce que bon
@@ -45,94 +47,22 @@ public class Main {
 		// }
 		// }
 		//
-		DBConnection db = DBConnection.getInstance();
+//		DBConnection db = DBConnection.getInstance();
 
+
+		// Frame
 		JFrame frame = new JFrame("NeOCampus");
-		frame.setPreferredSize(new Dimension(500, 500));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setPreferredSize(new Dimension(
+				GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDisplayMode().getWidth(),
+				GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDisplayMode()
+						.getHeight()));
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+//		frame.setPreferredSize(new Dimension(500,500));
+//		frame.setLocationRelativeTo(null);
 
-		JPanel panel = new JPanel();
-
-		JPanel p1 = new JPanel();
-		JPanel p2 = new JPanel();
-		JPanel p3 = new JPanel();
-		JPanel p4 = new JPanel();
-		JPanel p5 = new JPanel();
-		JPanel p6 = new JPanel();
-
-		JPanel p7 = new JPanel();
-		p1.setLayout(new GridBagLayout());
-		p2.setLayout(new GridBagLayout());
-		p3.setLayout(new GridBagLayout());
-		p4.setLayout(new GridBagLayout());
-		p5.setLayout(new GridBagLayout());
-		p6.setLayout(new GridBagLayout());
-		GridBagLayout gbl = new GridBagLayout();
-		GridBagConstraints gbcTitle = new GridBagConstraints();
-		gbcTitle.gridwidth = GridBagConstraints.RELATIVE;
-		gbcTitle.gridheight = GridBagConstraints.RELATIVE;
-//		gbcTitle.weightx = 1;
-//		gbcTitle.weighty = 0;
-//		gbcTitle.gridheight = 1;
-//
-		JButton b1 = new JButton("1");
-		JButton b2 = new JButton("2");
-		JButton b3 = new JButton("3");
-		JButton b4 = new JButton("4");
-		JButton b5 = new JButton("5");
-		JButton b6 = new JButton("6");
-		JButton b7 = new JButton("7");
-		JButton b8 = new JButton("8");
-
-//		b1.setPreferredSize(new Dimension(200, 50));
-//		b2.setPreferredSize(new Dimension(200, 50));
-//		b3.setPreferredSize(new Dimension(200, 50));
-//		b4.setPreferredSize(new Dimension(200, 50));
-//		b5.setPreferredSize(new Dimension(200, 50));
-//		b6.setPreferredSize(new Dimension(200, 50));
-//		b7.setPreferredSize(new Dimension(200, 50));
-//		b8.setPreferredSize(new Dimension(200, 50));
-
-		b1.setMinimumSize(new Dimension(200, 50));
-		b2.setMinimumSize(new Dimension(200, 50));
-		b3.setMinimumSize(new Dimension(200, 50));
-		b4.setMinimumSize(new Dimension(200, 50));
-		b5.setMinimumSize(new Dimension(200, 50));
-		b6.setMinimumSize(new Dimension(200, 50));
-		b7.setMinimumSize(new Dimension(200, 50));
-		b8.setMinimumSize(new Dimension(200, 50));
-		GridLayout gridLayout = new GridLayout(3, 2);
-		gridLayout.setVgap(50);
-		gridLayout.setHgap(50);
-		p1.add(b1, gbcTitle);
-		p2.add(b2, gbcTitle);
-		p3.add(b3, gbcTitle);
-		p4.add(b4, gbcTitle);
-		p5.add(b5, gbcTitle);
-		p6.add(b6, gbcTitle);
-
-		gbcTitle = new GridBagConstraints();
-		gbcTitle.anchor = GridBagConstraints.CENTER;
-		gbcTitle.fill = GridBagConstraints.HORIZONTAL;
-		gbcTitle.gridwidth = GridBagConstraints.REMAINDER;
-		gbcTitle.insets = new Insets(50, 0, 0, 0);
-		p7.setLayout(gbl);
-		p7.add(b7, gbcTitle);
-		p7.add(b8, gbcTitle);
-
-		JPanel p = new JPanel();
-		p.setLayout(new BorderLayout());
-		panel.add(p1);
-		panel.add(p2);
-		panel.add(p3);
-		panel.add(p4);
-		panel.add(p5);
-		panel.add(p6);
-
-		p.add(panel, BorderLayout.NORTH);
-		p.add(p7, BorderLayout.CENTER);
-		panel.setLayout(gridLayout);
-		frame.setContentPane(p);
+		frame.setContentPane(new FenetreServeur());
+		// centrage + affichage
 		frame.pack();
 		frame.setVisible(true);
 	}
--- src/view/FenetreServeur.java
+++ src/view/FenetreServeur.java
@@ -1,7 +1,8 @@
- package view;
+package view;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GraphicsEnvironment;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
@@ -13,115 +14,70 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class FenetreServeur extends JFrame implements ActionListener{
-	private JFrame frame;
-	private JPanel panel;
-	private JButton bouton1,bouton2,bouton3,bouton4,bouton5,bouton6,bouton7,bouton8;
-	
+public class FenetreServeur extends JPanel implements ActionListener {
+
+	private String[] nomsButtons = new String[] { "Ajouter un utilisateur", "Modifier un utilisateur",
+			"Supprimer un utilisateur", "Ajouter un groupe", "Modifier un groupe", "Supprimer un groupe",
+			"Ajouter un utilisateur à un groupe", "Supprimer un utilisateur d'un groupe" };
+
+	private JButton[] buttons = new JButton[8];
+	private JPanel[] panels = new JPanel[7];
+
+	private Dimension buttonsDimension = new Dimension(300, 50);
+
 	public FenetreServeur() {
-		//Frame
-		frame = new JFrame ("Interface Serveur");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(300, 600);
-		frame.setLocationRelativeTo(null);
-		//Panel
-		panel = new JPanel();
-		JPanel p1 = new JPanel();
-		JPanel p2 = new JPanel();
-		JPanel p3 = new JPanel();
-		JPanel p4 = new JPanel();
-		JPanel p5 = new JPanel();
-		JPanel p6 = new JPanel();
-		
-		JPanel p7 = new JPanel();
-		//boutons
-		// utilisateur
-		bouton1 = new JButton("Ajouter un utilisateur");
-		bouton2 = new JButton("Modifier un utilisateur");
-		bouton3 = new JButton("Supprimer un utilisateur");
-		//groupe 
-		bouton4 = new JButton("Ajouter un groupe");
-		bouton5 = new JButton("Modifier un groupe");
-		bouton6 = new JButton("Supprimer un groupe");
-		// user X grp
-		bouton7 = new JButton("Ajouter un utilisateur à un groupe");
-		bouton8 = new JButton("Supprimer un utilisateur d'un groupe");
-		// taille bouton
-		bouton1.setPreferredSize(new Dimension(300, 50));
-		bouton2.setPreferredSize(new Dimension(300, 50));
-		bouton3.setPreferredSize(new Dimension(300, 50));
-		bouton4.setPreferredSize(new Dimension(300, 50));
-		bouton5.setPreferredSize(new Dimension(300, 50));
-		bouton6.setPreferredSize(new Dimension(300, 50));
-		bouton7.setPreferredSize(new Dimension(300, 50));
-		bouton8.setPreferredSize(new Dimension(300, 50));
-		//Layout
-		p1.setLayout(new GridBagLayout());
-		p2.setLayout(new GridBagLayout());
-		p3.setLayout(new GridBagLayout());
-		p4.setLayout(new GridBagLayout());
-		p5.setLayout(new GridBagLayout());
-		p6.setLayout(new GridBagLayout());
-		GridBagLayout gbl = new GridBagLayout();
+		JPanel panel = new JPanel();
+		// Layout
+		GridLayout g = new GridLayout(3, 2);
+		g.setVgap(50);
+		g.setHgap(50);
+
+		panel.setLayout(g);
+
 		GridBagConstraints t = new GridBagConstraints();
 		t.anchor = GridBagConstraints.CENTER;
 		t.fill = GridBagConstraints.HORIZONTAL;
 		t.gridwidth = GridBagConstraints.REMAINDER;
-		t.anchor = GridBagConstraints.NORTH;
-		t.insets = new Insets(50, 0,0,0);
-		p7.setLayout(gbl);
-		
-		GridLayout g = new GridLayout(3,2);
-		g.setVgap(50);
-		g.setHgap(50);
-		//ActionListener
-		
-		bouton1.addActionListener(this);
-		bouton2.addActionListener(this);
-		bouton3.addActionListener(this);
-		bouton4.addActionListener(this);
-		bouton5.addActionListener(this);
-		bouton6.addActionListener(this);
-		bouton7.addActionListener(this);
-		bouton8.addActionListener(this);
-		//Ajout
-		p1.add(bouton1);
-		p2.add(bouton4);
-		p3.add(bouton3);
-		p4.add(bouton6);
-		p5.add(bouton2);
-		p6.add(bouton5);
-		p7.add(bouton7,t);
-		p7.add(bouton8,t);
-		JPanel p = new JPanel();
-		p.setLayout(new BorderLayout());
-		panel.add(p1);
-		panel.add(p2);
-		panel.add(p3);
-		panel.add(p4);
-		panel.add(p5);
-		panel.add(p6);
-		
-		p.add(panel,BorderLayout.NORTH);
-		p.add(p7,BorderLayout.CENTER);
-		panel.setLayout(g);
-		frame.add(p);
-		
-		//centrage + affichage
-		frame.pack();
-		frame.setVisible(true);
-		
-		
+		t.insets = new Insets(50, 0, 0, 0);
+
+		for (int i = 0; i < 8; i++) {
+			buttons[i] = new JButton(nomsButtons[i]);
+
+			if (i < 7) {
+				panels[i] = new JPanel();
+				panels[i].setLayout(new GridBagLayout());
+			}
+
+			// taille boutons
+			buttons[i].setPreferredSize(buttonsDimension);
+
+			// ActionListener
+			buttons[i].addActionListener(this);
+
+			if (i < 6)
+				panels[i].add(buttons[i]);
+			else if (i == 7) {
+				panels[i - 1].add(buttons[6], t);
+				panels[i - 1].add(buttons[7], t);
+			}
+
+			if (i < 6)
+				panel.add(panels[i]);
+		}
+
+		setLayout(new BorderLayout());
+
+		add(panel, BorderLayout.NORTH);
+		add(panels[6], BorderLayout.CENTER);
 	}
-	
+
 	public void actionPerformed(ActionEvent event) {
-		if( event.getSource() == bouton1) {
-			frame.setVisible(false);
-			new AjoutUtilisateur();
-		}
-		if(event.getSource() == bouton3) {
-			new SuppresionUtilisateur();
-		}
-		}
+//		if (event.getSource() == bouton1) {
+//			frame.setVisible(false);
+//			new AjoutUtilisateur();
+//		}
+//		if (event.getSource() == bouton3) {
+//			new SuppresionUtilisateur();
+//		}
+	}
 }
-	
