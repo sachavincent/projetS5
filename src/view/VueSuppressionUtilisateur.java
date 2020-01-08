aa18edf8b8baeea5a9c0878f5e121d4100050daf
Montels Embrun <arcta09@gmail.com>
2020-01-08 16:28:49 +0100

    font

--- src/view/VueSuppressionUtilisateur.java
+++ src/view/VueSuppressionUtilisateur.java
@@ -3,6 +3,7 @@ package view;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.util.Observable;
 import java.util.Observer;
@@ -10,11 +11,11 @@ import java.util.Observer;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import controller.SuppressionUtilisateurController;
 import database.DBConnection;
-import model.Utilisateur;
 
 //Interface permettant de supprimé un utilisateur via un menu déroulant
 public class VueSuppressionUtilisateur extends JPanel implements Observer {
@@ -22,20 +23,24 @@ public class VueSuppressionUtilisateur extends JPanel implements Observer {
 	private JButton ok = new JButton("OK");
 	private JButton annuler = new JButton("Annuler");
 	private JComboBox<String> listeUtilisateur;
-	private JPanel[] panel = new JPanel[4];
+	private JPanel[] panel = new JPanel[5];
+	private JLabel titrePanel = new JLabel("Suppression d'un utilisateur");
+	private JLabel titreComboBox = new JLabel("Sélection de l'utilisateur");
 
 	public VueSuppressionUtilisateur() {
 		// TODO pop up de confirmation quand on clique sur "OK"
 		// init
 		listeUtilisateur = new JComboBox<String>(
 				DBConnection.getInstance().getListeUtilisateurs().stream().map(g -> g.getNom()).toArray(String[]::new));
-		for (int i = 0; i < 4; i++) {
+		for (int i = 0; i < 5; i++) {
 			panel[i] = new JPanel();
 		}
+		Font f = titrePanel.getFont();
+		titrePanel.setFont(f.deriveFont(f.getStyle() | Font.ITALIC));
 		listeUtilisateur.setPreferredSize(new Dimension(300, 50));
 		
-		ok.setPreferredSize(new Dimension(300, 50));
-		annuler.setPreferredSize(new Dimension(300, 50));
+		ok.setPreferredSize(new Dimension(200, 50));
+		annuler.setPreferredSize(new Dimension(200, 50));
 		// actionListener
 		SuppressionUtilisateurController suppressionUtilisateurController = new SuppressionUtilisateurController();
 		ok.addActionListener(suppressionUtilisateurController);
@@ -47,14 +52,18 @@ public class VueSuppressionUtilisateur extends JPanel implements Observer {
 		panel[2].setLayout(new FlowLayout());
 		panel[3].setLayout(new BorderLayout());
 		// ajout
+		panel[1].add(titreComboBox);
 		panel[1].add(listeUtilisateur);
 		panel[2].add(ok);
 		panel[2].add(annuler);
+		panel[4].add(titrePanel);
+		panel[0].add(panel[4]);
 		panel[0].add(panel[1]);
 		panel[0].add(panel[2]);
 		panel[3].add(panel[0], BorderLayout.NORTH);
 
 		add(panel[3], BorderLayout.NORTH);
+
 
 	}
 
--- src/view/main.java
+++ src/view/main.java
@@ -7,7 +7,7 @@ import javax.swing.JFrame;
 
 public class main {
 	public static void main(String [] args) {
-		 new VueSuppressionUtilisateurGroupe();
+		 new VueSuppressionUtilisateur();
 
 	}
 
