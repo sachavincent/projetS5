2f7d46bf43ef371174ba23a94ec2b7b12bb245b1
sachavincent <Alykas88@gmail.com>
2020-01-09 15:44:36 +0100

    oopsie

--- src/controller/client/FenetreClientController.java
+++ src/controller/client/FenetreClientController.java
@@ -2,48 +2,142 @@ package controller.client;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.Set;
 
-import javax.swing.JFrame;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
-import javax.swing.SwingUtilities;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-import communication.ClientThread;
-import view.client.VueFenetreClient;
+import database.DBConnection;
+import model.Ticket;
+import model.Utilisateur.TypeUtilisateur;
 
-public class FenetreClientController implements ActionListener {
+public class FenetreClientController implements ActionListener, MouseListener {
 
-	private JTextField identifiantField;
-	private JPasswordField passwordField;
+	private ImageIcon openedTicketIcon = new ImageIcon("icons/opened_ticket.png", "Ticket ouvert");
+	private ImageIcon closedTicketIcon = new ImageIcon("icons/closed_ticket.png", "Ticket fermé");
 
-	public FenetreClientController(JTextField identifiantField, JPasswordField passwordField) {
-		this.identifiantField = identifiantField;
-		this.passwordField = passwordField;
+	private JLabel servicesAdmLabel;
+	private JLabel servicesTechLabel;
+	private JLabel secretariatLabel;
+
+	private JPanel panelAdm;
+
+	private Set<Ticket> tickets;
+
+	public FenetreClientController(JLabel servicesAdmLabel, JLabel servicesTechLabel, JLabel secretariatLabel,
+			JPanel panelAdm) {
+		this.servicesAdmLabel = servicesAdmLabel;
+		this.servicesTechLabel = servicesTechLabel;
+		this.secretariatLabel = secretariatLabel;
+
+		this.tickets = DBConnection.getInstance().getListeTickets();
+
+		this.panelAdm = panelAdm;
+	}
+
+	private void closeService(JLabel label) {
+		label.setIcon(closedTicketIcon);
+
+		System.out.println("closing");
+	}
+
+	private void openService(JLabel label) {
+		label.setIcon(openedTicketIcon);
+
+		if (label.equals(servicesAdmLabel)) {
+			System.out.println(tickets);
+			tickets.stream().forEach(t -> {
+				int idGroupe = t.getGroupeDestination().getIdGroupe();
+				TypeUtilisateur typeUtilisateur = DBConnection.getInstance().getListeAssociationsGroupeUtilisateur()
+						.stream().filter(agu -> agu.getGroupe().getIdGroupe() == idGroupe)
+						.map(agu -> agu.getUtilisateur().getType()).findFirst().orElse(null);
+
+				if (typeUtilisateur != null) {
+					System.out.println(typeUtilisateur);
+					if (typeUtilisateur == TypeUtilisateur.SERVICE_ADMINISTRATIF) {
+						this.panelAdm.add(new JLabel(t.getTitre()));
+						System.out.println("ajout");
+					}
+
+				}
+			});
+
+		}
+		System.out.println("opening");
+	}
+
+	private boolean isServiceOpened(JLabel label) {
+		return label.getIcon().equals(openedTicketIcon);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		String identifiant = identifiantField.getText();
-		String password = String.valueOf(passwordField.getPassword());
-		if (identifiant.isEmpty() || password.isEmpty())
-			return;
-
-		System.out.println("Click");
-		// TODO Encryption
-
-		ClientThread client = ClientThread.getClient();
-		if (client != null) {
-			boolean res = client.connect(identifiant, password);
-			if (res) {
-				System.out.println("true");
-				JFrame topFrame = (JFrame) SwingUtilities.getWindowAncestor(identifiantField);
-				topFrame.dispose();
-				
-				new VueFenetreClient();
-			}
-			// TODO Message d'erreur
+		if (e.getSource() instanceof JButton) {
 		}
-		// TODO Message d'erreur
+
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		if (e.getClickCount() == 2) {
+			if (e.getSource().equals(servicesAdmLabel)) {
+				if (isServiceOpened(servicesAdmLabel))
+					closeService(servicesAdmLabel);
+				else
+					openService(servicesAdmLabel);
+			} else if (e.getSource().equals(servicesTechLabel)) {
+				if (isServiceOpened(servicesTechLabel))
+					closeService(servicesTechLabel);
+				else
+					openService(servicesTechLabel);
+			} else if (e.getSource().equals(secretariatLabel)) {
+				if (isServiceOpened(secretariatLabel))
+					closeService(secretariatLabel);
+				else
+					openService(secretariatLabel);
+//			tickets.stream().forEach(t -> {
+//				int idGroupe = t.getGroupeDestination().getIdGroupe();
+//				TypeUtilisateur typeUtilisateur = DBConnection.getInstance().getListeAssociationsGroupeUtilisateur()
+//						.stream().filter(agu -> agu.getGroupe().getIdGroupe() == idGroupe)
+//						.map(agu -> agu.getUtilisateur().getType()).findFirst().orElse(null);
+//
+//				if (typeUtilisateur != null) {
+//					if (typeUtilisateur == TypeUtilisateur.SERVICE_ADMINISTRATIF) {
+//
+//					}
+//
+//				}
+//			});
+			}
+		}
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
