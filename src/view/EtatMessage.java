7e9c6e2c590ba928a5716fe5a14d20be2508581b
Montels Embrun <arcta09@gmail.com>
2020-01-09 23:21:39 +0100

    VueEtatMessage pas fini

--- src/view/EtatMessage.java
+++ src/view/EtatMessage.java
@@ -1,8 +1,11 @@
 package view;
 
-public class EtatMessage {
+import javax.swing.*;
+
+public class EtatMessage extends JFrame {
 	
-	public EtatMessage() {
+	public EtatMessage()  {
+		//TODO
 		//Recup liste Utilisateur dans le groupe
 		//Association Message Utilisateur pour savoir qui à lu ou non le messages
 		// affichage dans une fenetre du type : Utilisateur X = "Message Lu/Non Lu"
