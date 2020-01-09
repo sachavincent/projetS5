bef3b272da8355b363996b21ff84ea4adc2e0594
Montels Embrun <arcta09@gmail.com>
2020-01-09 23:18:38 +0100

    VueEtatMessage pas fini

--- src/view/EtatMessage.java
+++ src/view/EtatMessage.java
@@ -3,7 +3,10 @@ package view;
 public class EtatMessage {
 	
 	public EtatMessage() {
-		
+		//Recup liste Utilisateur dans le groupe
+		//Association Message Utilisateur pour savoir qui à lu ou non le messages
+		// affichage dans une fenetre du type : Utilisateur X = "Message Lu/Non Lu"
+			
 	}
 
 }
