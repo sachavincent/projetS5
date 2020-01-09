127b22f10fe0b3cec6581d4adea884b15c48d4fb
Montels Embrun <arcta09@gmail.com>
2020-01-09 13:32:47 +0100

    modif controller

--- src/view/main.java
+++ src/view/main.java
@@ -1,10 +1,12 @@
 package view;
 
 import view.client.VueFenetreClient;
+import view.server.VueFenetreServeur;
+import view.server.VueSuppressionGroupe;
 
 public class main {
 	public static void main(String[] args) {
-		new VueFenetreClient();
+		new VueSuppressionGroupe();
 	}
 
 }
